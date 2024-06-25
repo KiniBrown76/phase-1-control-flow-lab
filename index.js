1198d2ba29f77c5a14303e8343e70caf6b10000b
KiniBrown76 <esther.kini@student.moringaschool.com>
2024-06-25 08:57:43 +0300

    Add control lab

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(rideFeet){
   // Write your code here!
+  if (rideFeet<=400){
+    return 'This one is on me!';
+  }else if (rideFeet > 2000 && rideFeet <= 2500)
+    {
+      return 'I will gladly take your thirty bucks.';
+    } else if (rideFeet > 2500){
+      return 'No can do.';
+    } else{
+      return 'That will be twenty bucks.';
+    }
+  }
+
+function ternaryCheckCity(city) {
+  // Write your code here!
+  return city ==='NYC'? 'Ok, sounds good.' : 'No go.';
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch (tip){
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+      default:
+      return 'Bye.';
+  }
 }
